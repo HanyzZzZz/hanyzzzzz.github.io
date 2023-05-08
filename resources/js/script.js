b56d6996ffb47e1c6f82cb84349bbdc537e12d46
HanyzZz <121422084+HanyzZzZz@users.noreply.github.com>
2023-05-08 19:49:32 +0200

    js toggle dark mode console log

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,30 @@
-const document = document.getElementById("hero-card");
-function print (){
-    console.log('hero-card'); 
+document.getElementById("get-in-touch-socials").style.display ='none';
+
+function showSocials () {
+    const socials = document.getElementById("get-in-touch-socials");
+    const touch = document.getElementById("get-in-touch");
+    touch.style.display = 'none';
+    socials.style.display ='inline-block';
+}
+
+
+function showGetInTouch () {
+    const socials = document.getElementById("get-in-touch-socials");
+    const touch = document.getElementById("get-in-touch");
+    socials.style.display ='none';
+    touch.style.display = 'inline-block';
+}
+
+// //////////////////////////===DARK MODE===//////////////////////////
+let darkMode = false;
+function toggleDarkMode (){
+    if(darkMode === true) {
+        
+        console.log('darkmode ' + darkMode);
+        return darkMode = false; 
+    } else {
+        document.body.style.color = 'red';
+        console.log('darkmode ' + darkMode);
+        return darkMode = true;
+    }
 }
