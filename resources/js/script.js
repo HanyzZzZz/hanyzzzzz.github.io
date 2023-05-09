616826abe1e657d1ba7ae0f88d7b210e7725283a
HanyzZz <121422084+HanyzZzZz@users.noreply.github.com>
2023-05-09 05:42:09 +0200

    dokonceni dark/light modu. Zruseni prvniho radku scriptu. Ostatni stranky na darkmode hazely error.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-document.getElementById("get-in-touch-socials").style.display ='none';
+// document.getElementById("get-in-touch-socials").style.display ='none';
 
 function showSocials () {
     const socials = document.getElementById("get-in-touch-socials");
@@ -20,11 +20,62 @@ let darkMode = false;
 function toggleDarkMode (){
     if(darkMode === true) {
         document.body.style.backgroundColor = '#ffffff';
+        document.getElementById('darkMode').style.display = 'block';
+        document.getElementById('lightMode').style.display = 'none';
+        for(let i = 0; i < document.getElementsByTagName('h1').length; i++){
+            document.getElementsByTagName('h1')[i].style.color = '#202124';
+        }
+        for(let i = 0; i < document.getElementsByTagName('h2').length; i++){
+            document.getElementsByTagName('h2')[i].style.color = '#202124';
+        }
+        for(let i = 0; i < document.getElementsByTagName('h3').length; i++){
+            document.getElementsByTagName('h3')[i].style.color = '#202124';
+        }
+        for(let i = 0; i < document.getElementsByTagName('h4').length; i++){
+            document.getElementsByTagName('h4')[i].style.color = '#202124';
+        }
+        for(let i = 0; i < document.getElementsByTagName('h5').length; i++){
+            document.getElementsByTagName('h5')[i].style.color = '#202124';
+        }
+        for(let i = 0; i < document.getElementsByTagName('p').length; i++){
+            document.getElementsByTagName('p')[i].style.color = '#202124';
+        }
+        for(let i = 0; i < document.getElementsByTagName('a').length; i++){
+            document.getElementsByTagName('a')[i].style.color = '#202124';
+        }
+        for(let i = 0; i < document.getElementsByClassName('material-symbols-outlined').length; i++){
+            document.getElementsByClassName('material-symbols-outlined')[i].style.color = '#202124';
+        }
         console.log('darkmode ' + darkMode);
-        
         return darkMode = false; 
     } else {
         document.body.style.backgroundColor = '#10162f';
+        document.getElementById('darkMode').style.display = 'none';
+        document.getElementById('lightMode').style.display = 'block';
+        for(let i = 0; i < document.getElementsByTagName('h1').length; i++){
+            document.getElementsByTagName('h1')[i].style.color = 'white';
+        }
+        for(let i = 0; i < document.getElementsByTagName('h2').length; i++){
+            document.getElementsByTagName('h2')[i].style.color = 'white';
+        }
+        for(let i = 0; i < document.getElementsByTagName('h3').length; i++){
+            document.getElementsByTagName('h3')[i].style.color = 'white';
+        }
+        for(let i = 0; i < document.getElementsByTagName('h4').length; i++){
+            document.getElementsByTagName('h4')[i].style.color = 'white';
+        }
+        for(let i = 0; i < document.getElementsByTagName('h5').length; i++){
+            document.getElementsByTagName('h5')[i].style.color = 'white';
+        }
+        for(let i = 0; i < document.getElementsByTagName('p').length; i++){
+            document.getElementsByTagName('p')[i].style.color = 'white';
+        }
+        for(let i = 0; i < document.getElementsByTagName('a').length; i++){
+            document.getElementsByTagName('a')[i].style.color = 'white';
+        }
+        for(let i = 0; i < document.getElementsByClassName('material-symbols-outlined').length; i++){
+            document.getElementsByClassName('material-symbols-outlined')[i].style.color = 'white';
+        }
         console.log('darkmode ' + darkMode);
         return darkMode = true;
     }
